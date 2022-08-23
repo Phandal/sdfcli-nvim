3ab4bf71cd4e0314e3ca4c3f459084b544c23f93
Phandal <bfeld.111@gmail.com>
2022-08-22 21:24:50 -0500

    Changed util to utils and added the command

--- lua/sdfcli-nvim/commands/create_project.lua
+++ lua/sdfcli-nvim/commands/create_project.lua
@@ -1,27 +1,27 @@
-local util = require('sdfcli-nvim.utils')
+local utils = require('sdfcli-nvim.utils')
 local config = require('sdfcli-nvim.config')
 
 local M = {}
 
 M.create_project = function()
   if not config.opts.sdf_installed then
-    util.error_log("Cannot find 'sdfcli' in your $PATH")
+    utils.error_log("Cannot find 'sdfcli' in your $PATH")
     return
   end
-  local cwd = util.get_cwd()
-  util.clear_prompt()
+  local cwd = utils.get_cwd()
+  utils.clear_prompt()
   local input_opts = { prompt = 'Enter path to project: ', default = cwd, completion = 'file' }
   vim.ui.input(input_opts, function(project_path)
-    if not project_path or util.file_exists(project_path) then
-      util.clear_prompt()
-      util.warn_log('That folder already exists!')
+    if not project_path or utils.file_exists(project_path) then
+      utils.clear_prompt()
+      utils.warn_log('That folder already exists!')
       return
     end
 
-    local project_dir = util.get_base_path(project_path)
-    local project_name = util.get_folder_name(project_path)
+    local project_dir = utils.get_base_path(project_path)
+    local project_name = utils.get_folder_name(project_path)
 
-    util.clear_prompt()
+    utils.clear_prompt()
     print(vim.fn.system(config.opts.sdfcli_cmd .. ' createproject -type ACCOUNTCUSTOMIZATION -parentdirectory ' .. project_dir .. ' -projectname ' .. project_name))
   end)
 end
